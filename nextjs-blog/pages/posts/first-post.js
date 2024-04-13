7be0f551946a3cbc08d726710f699d77e8c3f35b
강석원 <lg@gangseog-won-ui-MacBookPro.local>
2024-04-14 00:48:46 +0900

    iwannasleep

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Professor Youngjoon Won, assignment is too gahok. </h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
